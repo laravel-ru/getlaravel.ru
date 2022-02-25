0f1b1cf7e6dce1bd8ce8e968b2ee82f74769884e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-02-25 22:21:45 +0300

    Перевод макета

--- resources/views/partials/layout.blade.php
+++ resources/views/partials/layout.blade.php
@@ -11,21 +11,21 @@
     @endif
 
     <!-- Primary Meta Tags -->
-    <meta name="title" content="Laravel - The PHP Framework For Web Artisans">
-    <meta name="description" content="Laravel is a PHP web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.">
+    <meta name="title" content="Laravel - PHP-фреймворк для веб-мастеров">
+    <meta name="description" content="Laravel — это среда веб-приложений PHP с выразительным и элегантным синтаксисом. Мы уже заложили основу — даем вам возможность творить, не беспокоясь о мелочах.">
 
     <!-- Open Graph / Facebook -->
     <meta property="og:type" content="website">
     <meta property="og:url" content="https://laravel.com/">
-    <meta property="og:title" content="Laravel - The PHP Framework For Web Artisans">
-    <meta property="og:description" content="Laravel is a PHP web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.">
+    <meta property="og:title" content="Laravel - PHP-фреймворк для веб-мастеров">
+    <meta property="og:description" content="Laravel — это среда веб-приложений PHP с выразительным и элегантным синтаксисом. Мы уже заложили основу — даем вам возможность творить, не беспокоясь о мелочах.">
     <meta property="og:image" content="https://laravel.com/img/og-image.jpg">
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="https://laravel.com/">
-    <meta property="twitter:title" content="Laravel - The PHP Framework For Web Artisans">
-    <meta property="twitter:description" content="Laravel is a PHP web application framework with expressive, elegant syntax. We’ve already laid the foundation — freeing you to create without sweating the small things.">
+    <meta property="twitter:title" content="Laravel - PHP-фреймворк для веб-мастеров">
+    <meta property="twitter:description" content="Laravel — это среда веб-приложений PHP с выразительным и элегантным синтаксисом. Мы уже заложили основу — даем вам возможность творить, не беспокоясь о мелочах.">
     <meta property="twitter:image" content="https://laravel.com/img/og-image.jpg">
 
     <!-- Favicon -->
